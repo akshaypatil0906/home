43c85aeb4890e9c483e75473bb1475c2fbbecc91
Akshay Patil <akshay@bond.ai>
2024-03-30 00:35:38 +0530

    added new project

---
+++ src/content_option.js
@@ -0,0 +1,175 @@
+const logotext = "Home";
+const meta = {
+    title: "Akshay Patil",
+    description: "I’m Akshay Patil data scientist | AI/ML Developer, currently working in Pune",
+};
+
+const introdata = {
+    title: "I’m Akshay Patil",
+    animated: {
+        first: "I love coding",
+        second: "I code to Live",
+        third: "I Work on Data driven Technologies",
+    },
+    description: "Elevating the Future with AI and Machine Learning Wizardry",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Welcome to the realm of boundless innovation and intelligent algorithms! I'm Akshay Patil, an AI/ML Developer with a passion for crafting cutting-edge solutions that redefine possibilities. With a keen eye for detail and a knack for problem-solving, I thrive in the ever-evolving landscape of artificial intelligence and machine learning.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Machine Learning",
+        value: 85,
+    },
+    {
+        name: "NLP",
+        value: 85,
+    },
+    {
+        name: "Deep Learning",
+        value: 70,
+    },
+    {
+        name: "AWS",
+        value: 60,
+    },
+    {
+        name: "MLOPS",
+        value: 80,
+    },
+];
+
+const services = [{
+        title: "Machine Learning Model Development",
+        description: "Design, develop, and deploy custom machine learning models tailored to specific business needs. This includes regression, classification, clustering, and other advanced algorithms.",
+    },
+    {
+        title: "Natural Language Processing (NLP) Solutions",
+        description: "Develop NLP solutions to process and analyze text data, including sentiment analysis, text classification, named entity recognition, and text summarization.",
+    },
+    {
+        title: "Recommendation Systems",
+        description: "Build recommendation systems for personalized content, product recommendations, and user-based or item-based collaborative filtering.",
+    },
+    {
+        title: "Custom AI Solutions",
+        description: "Develop custom AI solutions tailored to specific industry verticals, including healthcare, finance, e-commerce, manufacturing, and more, to address unique business challenges and opportunities."
+    }
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Sentiment Analysis Web App",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Chatbot with Natural Language Understanding",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Bond Label Prediction",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "akshayspatil0906@gmail.com",
+    YOUR_FONE: "+91 7796574668",
+    description: "connect with me for any queries",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/akshaypatil0906",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/akshay-patil0906/",
+    twitter: "https://twitter.com",
+    hackerrank: "https://www.hackerrank.com/profile/akshayspatil0906"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
